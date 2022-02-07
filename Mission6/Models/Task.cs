a6fba5271fb45ab969b25b86191fe94a5a183f9d
davidshipley <dshiple2@byu.edu>
2022-02-07 11:25:07 -0700

    this time I saved :)

--- Mission6/Models/Task.cs
+++ Mission6/Models/Task.cs
@@ -1,10 +1,20 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace Mission6.Models
 {
     public class Task
     {
-        public Task()
-        {
-        }
+        [Key]
+        [Required]
+        public int TaskID { get; set; }
+        [Required]
+        public string TaskName { get; set; }
+        [DataType(DataType.Date)]
+        public string DueDate { get; set; }
+        [Required]
+        public int Quadrant { get; set; }
+        public string Category { get; set; }
+        public bool Completed { get; set; }
     }
 }
--- Mission6/Models/TaskContext.cs
+++ Mission6/Models/TaskContext.cs
@@ -1,10 +1,15 @@
 ﻿using System;
+using Microsoft.EntityFrameworkCore;
+
 namespace Mission6.Models
 {
-    public class TaskContext
+    public class TaskContext : DbContext
     {
-        public TaskContext()
+        public TaskContext(DbContextOptions<TaskContext> options) : base (options)
         {
+            //leave blank
         }
+
+        public DbSet<Task> tasks { get; set; }
     }
 }
