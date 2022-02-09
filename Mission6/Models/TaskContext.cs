c300531ee99d551ad56f8bd3a730a90d480d78fe
davidshipley <dshiple2@byu.edu>
2022-02-09 08:19:45 -0700

    seeded data for real

--- Mission6/Models/TaskContext.cs
+++ Mission6/Models/TaskContext.cs
@@ -10,9 +10,18 @@ namespace Mission6.Models
             //leave blank
         }
 
-        public DbSet<Task> tasks { get; set; }
+        public DbSet<Task> Tasks { get; set; }
+        public DbSet<Category> Categories {get; set;}
 
-        public DbSet<Task> addedTasks { get; set; }
 
+        protected override void OnModelCreating(ModelBuilder mb)
+        {
+            mb.Entity<Category>().HasData(
+                new Category { CategoryID = 1, CategoryName = "Home" },
+                new Category { CategoryID = 2, CategoryName = "School" },
+                new Category { CategoryID = 3, CategoryName = "Work" },
+                new Category { CategoryID = 4, CategoryName = "Church" }
+                );
+        }
     }
 }
